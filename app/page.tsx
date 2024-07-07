a828177db2b1f79aea1f22c772b6a6a4702c4935
Favour <jammoben@gmail.com>
2024-07-08 00:44:00 +0100

    Everyone can have atleast 3 straight resting days a month, Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+//instructions;
+// 1. Each day is a column
+// 2. Each day can only have n drivers, 2 People Resting and 1 Night Shift
+// 3. Every 7 days or a week we can only have 3 drivers, 3 people resting and 1 night shift
+// 4. during the any weekends one column has to have at least 3 straight resting days. Friday, Saturday, Sunday
+// 5. Drivers cannot have more that 2 consecutive days with unless it is step 4 above
+
 import { useEffect } from "react";
 
 export default function Home() {
@@ -32,8 +39,31 @@ function generateRandom(): string {
   return ["D", "R", "N"][index];
 }
 
-function rowAndColumnIsWeekEndDay(row: number, column: number): boolean {
-  return false;
+function isWeekEnd(column: number, weekEndDays: Map<number, boolean>): boolean {
+  return weekEndDays.get(column) === false;
+}
+function update3StraightRestingDays(
+  data: DataObject,
+  rowLength: number,
+  columnLength: number
+) {
+  let weekEndDays = new Map<number, boolean>();
+  for (let k = 4; k < numberOfDaysInAMonth; k += 7) {
+    weekEndDays.set(k, false);
+  }
+  for (let row = 0; row < rowLength; row++) {
+    for (let column = 0; column < columnLength; column++) {
+      if (isWeekEnd(column, weekEndDays) && data[row][column] === "0") {
+        weekEndDays.set(column, true);
+        data[row][column] = "R";
+        data[row][column + 1] = "R";
+        data[row][column + 2] = "R";
+        console.log(column, column + 1, column + 2);
+        break;
+      }
+    }
+    console.log(weekEndDays);
+  }
 }
 function fixBoard({
   data,
@@ -46,24 +76,30 @@ function fixBoard({
   column: number;
   valueHashMap: valueHashMapType;
 }) {
+  if (data[row][column] !== "0") {
+    return;
+  }
   console.log(row, column);
   console.log("............................");
   const value: string = generateRandom();
+  let allGood = false;
   // this checks for columns only
   for (let i = 0; i < row; i++) {
     if (valueHashMap[value].R_count < valueHashMap[value].rowLimit) {
       valueHashMap[value].R_count++;
+      allGood = true;
       data[row][column] = value;
       break;
     }
   }
   if (data[row][column] === "0") {
+    allGood = true;
     data[row][column] = value;
   }
 
   // this checks for rows now
 
-  for (let j = column; j > 7; j--) {
+  for (let j = column; j >= 0; j--) {
     if (valueHashMap[value].C_count < valueHashMap[value].colLimit) {
       valueHashMap[value].C_count++;
       data[row][column] = value;
@@ -72,35 +108,29 @@ function fixBoard({
   }
 }
 
-function Zeroes(value: number): string[] {
-  const results: string[] = [];
-  for (let i = 0; i < value; i++) {
-    results.push("0");
-  }
-
-  return results;
-}
 function Table() {
   const data: DataObject = {
-    0: Zeroes(numberOfDaysInAMonth),
-    1: Zeroes(numberOfDaysInAMonth),
-    2: Zeroes(numberOfDaysInAMonth),
-    3: Zeroes(numberOfDaysInAMonth),
-    4: Zeroes(numberOfDaysInAMonth),
-    5: Zeroes(numberOfDaysInAMonth),
+    0: Array(numberOfDaysInAMonth).fill("0"),
+    1: Array(numberOfDaysInAMonth).fill("0"),
+    2: Array(numberOfDaysInAMonth).fill("0"),
+    3: Array(numberOfDaysInAMonth).fill("0"),
+    4: Array(numberOfDaysInAMonth).fill("0"),
+    5: Array(numberOfDaysInAMonth).fill("0"),
   };
 
   const rowLength: number = Object.keys(data).length;
   const columnLength: number = data[0].length;
 
+  update3StraightRestingDays(data, rowLength, columnLength);
   for (let column = 0; column < columnLength; column++) {
     const valueHashMap: valueHashMapType = {
       D: { R_count: 0, C_count: 0, rowLimit: 3, colLimit: 3 },
       R: { R_count: 0, C_count: 0, rowLimit: 2, colLimit: 3 },
       N: { R_count: 0, C_count: 0, rowLimit: 1, colLimit: 1 },
     };
+
     for (let row = 0; row < rowLength; row++) {
-      fixBoard({ data, row, column, valueHashMap });
+      //fixBoard({ data, row, column, valueHashMap });
     }
   }
   console.log(data);
